8ecea31682d75710679ea64ba639784c484e57d2
unknown <arthurguilherme2013@gmail.com>
2023-04-21 07:43:17 +0100

    Studying more about map, filter, reduce and practicing more.

--- Dominating_React/1_Reviewing_ECMASCRIPT6/Practice_Data_Transformation/script.js
+++ Dominating_React/1_Reviewing_ECMASCRIPT6/Practice_Data_Transformation/script.js
@@ -17,3 +17,44 @@ const sum = numbers.reduce(function(acumulator, actualValue) {
     return acumulator + actualValue;
 });
 console.log("Without Initial Value", sum);
+// Form 2 - With Initial Value
+const sum2 = numbers.reduce(function(acumulator, actualValue) {
+    console.log(acumulator, actualValue);
+    return acumulator + actualValue
+}, 12);
+console.log("Now, with initial value", sum2);
+
+
+/* Study better this topic after!
+
+
+console.log('________________Compose______________');
+const add = x => x + 1;
+
+const multiply = x => x * 2;
+
+const testCompose = (func1, func2) => {
+    return function(value) {
+        return func1(func2(value));
+    };
+};
+
+console.log(testCompose(2));*/
+
+console.log('________________Recursion______________');
+function factorial(n) {
+    if(n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    };
+};
+
+console.log(factorial(6))
+
+console.log('________________Currying______________');
+const add = x => y => x + y;
+let increment = add(1);
+console.log(increment(2));
+
+// p.586 fibonacci
